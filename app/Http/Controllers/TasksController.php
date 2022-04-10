6779038bc39b54b2ef2af3304ed4793d655f2e05
miki.yamamoto <miki.yamamoto11@outlook.jp>
2022-04-10 06:56:46 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -47,8 +47,15 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
-         // タスクを作成
+        //バリデーション
+        $request->validate([
+            'status'=>'required|max:10',
+            'content'=>'required|max:255',
+        ]);
+
+        // タスクを作成
         $task = new Task;
+        $task->status=$request->status;
         $task->content = $request->content;
         $task->save();
 
@@ -99,9 +106,16 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+         //バリデーション
+        $request->validate([
+            'status'=>'required|max:10',
+            'content'=>'required|max:255',
+        ]);
+       
         // idの値でメッセージを検索して取得
         $task = Task::findOrFail($id);
         // タスクを更新
+       $task->status=$request->status; 
         $task->content = $request->content;
         $task->save();
 
